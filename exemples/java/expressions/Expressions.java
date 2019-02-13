1df9edf20fa35d08e491ed0bdd02a2013193656a
Thierry <tigraff@gmail.com>
2019-02-13 12:05:51 +0100

    Remanie cours intro java, ajout exo bissextile

--- exemples/java/expressions/Expressions.java
+++ exemples/java/expressions/Expressions.java
@@ -4,11 +4,21 @@ class Expressions{
         int a;
         int b = (a=3);
         System.out.println("a=3 \t: " + b);
-
+        
+        System.out.println("------------------");
+        if((a = 2) == 3){
+            System.out.println("a = 3");
+        } else {
+            System.out.println("a != 3");
+        }
+        System.out.println("a = " + a);
+        
+        System.out.println("------------------");
         int x = 4;
         int y = (x *= 2);
         System.out.println("x *= 2 \t: " + y);
         
+        System.out.println("------------------");
         int i = 10;
         int j;
         j = (i++);
