6e89f5c3dbafbbf43620b608a838823a07071f7d
Thierry <tigraff@gmail.com>
2019-02-19 23:15:39 +0100

    Exo Boucles + remanie cours java

---
+++ exos/java/boucles/Boucles.java
@@ -0,0 +1,26 @@
+class Boucles{
+
+    public static void main(String[] args) {
+        System.out.println("=== Avec for ===");
+        avecFor();
+        
+        System.out.println("=== Avec do ===");
+        avecDo();
+        
+        System.out.println("=== Avec while ===");
+        avecWhile();
+    }
+    
+    /** Affiche les nombres de 1 à 20 en utilsant for **/
+    private static void avecFor(){
+    }
+    
+    /** Affiche les nombres de 1 à 20 en utilsant do **/
+    private static void avecDo(){
+    }
+    
+    /** Affiche les nombres de 1 à 20 en utilsant while **/
+    private static void avecWhile(){
+    }
+    
+}
