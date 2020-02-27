d5c8e9cc16f80980fe60eb20206151e27494b3da
Thierry <tigraff@gmail.com>
2020-02-27 08:23:26 +0100

    Fix typos + ajout exemples/java/constructeur/

---
+++ exemples/java/constructeur/Construct.java
@@ -0,0 +1,25 @@
+/** 
+    Teste les conversions implicites effectuées lors de l'appel de this()
+    Pour tester, commenter les différents constructeurs pour voir lequel est appelé
+**/
+class Construct {
+    
+    public static void main(String[] args) {
+        Test t1 = new Test();
+    }
+    
+    public Test(){
+        this(1);
+        //this(1.0);
+        //this(1F);
+    }
+    public Test(int x){
+        System.out.println("Constructeur(int)");
+    }
+    public Test(double x){
+        System.out.println("Constructeur(double)");
+    }
+    public Test(float x){
+        System.out.println("Constructeur(float)");
+    }
+}
