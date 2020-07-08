16c5afd824e58b2097fd3da8bdd517e5daf17487
Thierry <tigraff@gmail.com>
2020-07-08 09:12:34 +0200

    Remets le code de départ pour exo ducks

---
+++ exos/patterns/mix/ducks/1-depart/DuckSimulator.java
@@ -0,0 +1,21 @@
+
+public class DuckSimulator {
+    
+    public void simulate(){
+		Quackable mallardDuck = new MallardDuck();
+		Quackable redheadDuck = new RedheadDuck();
+		Quackable duckCall = new DuckCall();
+		Quackable rubberDuck = new RubberDuck();
+ 
+		System.out.println("\n=== Duck Simulator ===");
+ 
+		simulate(mallardDuck);
+		simulate(redheadDuck);
+		simulate(duckCall);
+		simulate(rubberDuck);
+	}
+   
+	public void simulate(Quackable duck) {
+		duck.quack();
+	}
+}
