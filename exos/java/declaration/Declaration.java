c572a5f085f163a6f8c93e97a0e2d894a6b9c379
Thierry <tigraff@gmail.com>
2021-09-26 12:30:49 +0200

    Ajout exo 'déclaration' ; début page install avec VM

---
+++ exos/java/declaration/Declaration.java
@@ -0,0 +1,46 @@
+class Declaration{
+
+    public static void main(String[] args) {
+        
+        // Déclarez 2 variables, a et b
+        int a, b;
+        
+        // Essayez d'afficher a en utilisant System.out.println();
+        // Compilez votre code et constatez que javac ne veut pas compiler.
+        // Lire et comprendre le messsage d'erreur
+        // Commentez la ligne qui génère le message d'erreur
+        
+        // Affectez à a la valeur 2
+        
+        // Affichez a en utilisant System.out.println();
+        // Le résultat doit être :
+        // a = 2
+        
+        // Affichez une ligne vide
+        
+        // Donnez à b la valeur de a
+        // Affichez a et b en utilisant System.out.println();
+        // Le résultat doit être :
+        // a = 2
+        // b = 2
+        
+        // Affichez une ligne vide
+        
+        // Augmentez de 10 la valeur de a
+        // Affichez a et b en utilisant System.out.println();
+        // Le résultat doit être :
+        // a = 12
+        // b = 2
+        
+        // Affichez une ligne vide
+        
+        // Déclarez et initialisez une variable s de type String ayant comme valeur "toto"
+        // (la déclaration et l'initialisation doivent être effectuées en une seule instruction)
+        // Affichez la valeur de s en utilisant System.out.println();
+        // Le résultat doit être :
+        // s = "toto"
+        
+        
+    }
+    
+}
