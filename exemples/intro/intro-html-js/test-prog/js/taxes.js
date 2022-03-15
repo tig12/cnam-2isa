5b327dfbed3ae68cbdce1bc657e21619ecd63934
Thierry Graff <tigraff@gmail.com>
2022-03-15 12:05:29 +0100

    Ajout exercices pour intro js html

--- exemples/intro/intro-html-js/test-prog/js/taxes.js
+++ exemples/intro/intro-html-js/test-prog/js/taxes.js
@@ -1,10 +1,14 @@
 /** 
     Calcule le prix TTC à partir d'un prix hors taxe.
-    Le pourcentage de taxe est de 20%
+    Le pourcentage de taxe est de 20%.
     @param  prixHT  Prix hors taxe
     @return prixHT augmenté de 20%
 **/
 function calculerPrixTTC_20pourcent(prixHT){
     let prixTTC = prixHT * 1.2;
     return prixTTC;
+}
+
+function calculerPrixTTC(prixHT, taux){
+	return prixHT * (100 + taux) / 100;
 }
