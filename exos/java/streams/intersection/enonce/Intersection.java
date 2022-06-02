29f570ce508a7df0c55d1790306149c58a47011c
Thierry <tigraff@gmail.com>
2022-06-02 08:04:28 +0200

    Divers petits ajouts

--- exos/java/streams/intersection/enonce/Intersection.java
+++ exos/java/streams/intersection/enonce/Intersection.java
@@ -5,30 +5,32 @@ import java.util.stream.Collectors;
 
 public class Intersection{
     
+    public static String[] list1 = {
+        "Carotte",
+        "Chou",
+        "Navet",
+        "Patate",
+        "Radis",
+        "Tomate"
+    };
+    public static String[] list2 = {
+        "Radis",
+        "Banane",
+        "Pomme",
+        "Poire",
+        "Prune",
+        "Tomate",
+    };
+        
     public static void main(String[] args){
-        String[] list1 = {
-            "Carotte",
-            "Chou",
-            "Navet",
-            "Patate",
-            "Radis",
-            "Tomate"
-        };
-        String[] list2 = {
-            "Radis",
-            "Banane",
-            "Pomme",
-            "Poire",
-            "Prune",
-            "Tomate",
-        };
-        System.out.println(intersection1(list1, list2));
-        System.out.println(intersection2(list1, list2));
+        System.out.println("intersection1\t" + intersection1(list1, list2));
+        System.out.println("intersection2\t" + intersection2(list1, list2));
+        System.out.println("intersection3\t" + intersection3(list1, list2));
     }
     
     /** 
         Renvoie une liste contenant les chaînes présentes à la fois dans <code>list1</code> et <code>list2</code>.
-        Utilise les itérations classiques
+        Utilise les itérations classiques (boucles for sur tableaux avec index).
     **/
     public static List<String> intersection1(String[] list1, String[] list2){
         // A implémenter
@@ -36,9 +38,17 @@ public class Intersection{
     
     /** 
         Renvoie une liste contenant les chaînes présentes à la fois dans <code>list1</code> et <code>list2</code>.
-        Utilise l'API streams
+        Les tableaux sont convertis en ArrayList, utilise une boucle "foreach" pour itérer.
     **/
     public static List<String> intersection2(String[] list1, String[] list2){
+        // A implémenter
+    }
+    
+    /** 
+        Renvoie une liste contenant les chaînes présentes à la fois dans <code>list1</code> et <code>list2</code>.
+        Utilise l'API streams
+    **/
+    public static List<String> intersection3(String[] list1, String[] list2){
         // A implémenter
     }
     
