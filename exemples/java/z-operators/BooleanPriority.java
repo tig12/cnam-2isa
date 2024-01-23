4a8661e7f33afa90914d130c88d2c9e9631c43e0
Thierry Graff <tigraff@gmail.com>
2024-01-23 10:10:48 +0100

    Améliore explications dans l'exercice multiplication circulaire

---
+++ exemples/java/z-operators/BooleanPriority.java
@@ -0,0 +1,16 @@
+/** 
+    Illustre le fait que && a une priorité supérieure à ||
+
+**/
+class BooleanPriority{
+    
+    public static void main(String[] args){
+        int a, b, c;
+        
+        
+    }
+    
+    private static test(int a, int b, int c){
+    
+    }
+}
